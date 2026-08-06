a1ac625b790964c3d4d329b54ffced1363117d59
juliovalero05 <juliorapu@outlook.com>
2026-08-05 20:58:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,8 @@
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-from kivy.clock import Clock
-from kivy.core.window import Window
-from kivy.uix.camera import Camera
-from plyer import tts
-import requests
+from kivy.uix.label import Label
 
-# Permisos automaticos
-try:
-    from android.permissions import request_permissions, Permission
-    request_permissions([Permission.CAMERA, Permission.RECORD_AUDIO, Permission.INTERNET])
-except:
-    pass
+class SolApp(App):
+    def build(self):
+        return Label(text="Hola Sol, si ves esto ya jalo!")
 
-try:
-    from plyer import stt
-    TIENE_OIDOS = True
-except:
-    TIENE_OIDOS = False
-
-Window.clearcolor = (0, 0, 0, 1)
-
-KV = '''
-<SolFinal>:
-    orientation: 'vertical'
-    FloatLayout:
-        Camera:
-            id: cam
-            index: 0
-            resolution: (1280, 720)
-            play: True
-            size_hint: 1, 1
-            pos_hint: {'center_x': 
+SolApp().run()
